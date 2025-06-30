6e91131574d6db7bb60af79633ae4ac1a689181a
karam-koujan <karamkaku2000@gmail.com>
2025-06-30 17:14:39 +0100

    test

--- cpp0/ex01/main.cpp
+++ cpp0/ex01/main.cpp
@@ -28,7 +28,7 @@ bool    is_valid_cmd(std::string cmd)
     return false;
 }
 
-void    handle_add(PhoneBook phonebook)
+void    handle_add(PhoneBook& phonebook)
 {
     Contact contact;
     while (contact.firstname.length() == 0)
@@ -71,23 +71,76 @@ void    handle_add(PhoneBook phonebook)
     std::cout << "A Contact added to phone number successfully!!" << std::endl;
 }
 
+void	print_cols(std::string field)
+{
+	int i = 10;
+	i-= field.length();
+	if (i < 0)
+		std::cout << field.substr(0, 9) + ".";
+	else
+		std::cout << std::string(i, '*') << field;
+}
+
+void handle_search(PhoneBook& phonebook)
+{
+/*
+index, first name, last
+name and nickname.
+◦ Each column must be 10 characters wide. A pipe character (’|’) separates
+*/
+	int	i = 0;
+	int	idx = 0;
+	while (i < phonebook.contact_num)
+	{
+		print_cols(std::to_string(i + 1));
+		std::cout << "|";
+		print_cols(phonebook.contacts[i].firstname);
+		std::cout << "|";
+		print_cols(phonebook.contacts[i].lastname);
+		std::cout << "|";
+		print_cols(phonebook.contacts[i].nickname);
+		std::cout << std::endl;
+		i++;
+	}
+	std::cout << "Enter contact index: ";
+	std::cin >> idx;
+	if (idx <= 0 || idx > phonebook.contact_num)
+		std::cout << "Index out of range" << std::endl;
+	else
+	{
+		print_cols(std::to_string(idx + 1));
+		print_cols(phonebook.contacts[idx].firstname);
+		print_cols(phonebook.contacts[idx].lastname);
+		print_cols(phonebook.contacts[idx].nickname);
+		std::cout << std::endl;		
+	}
+}
+
 int main()
 {
     // prompt the user to enter one of three commands, ADD, SEARCH, EXIT
     //bool exit = true;
     PhoneBook phonebook;
-    std::string cmd;
-    std::cout << "Please Enter three commands: ADD, SEARCH, EXIT" << std::endl;
-    std::cout << "Command : ";
-    std::cin >> cmd;
-    if (!is_valid_cmd(cmd))
-        std::cout << "wrong command!!";
-    else
-    {
-        if (cmd == "ADD")
-        {
-            handle_add(phonebook);
-        }
-    }
+	while (true)
+	{
+
+		std::string cmd;
+		std::cout << "Please Enter three commands: ADD, SEARCH, EXIT" << std::endl;
+		std::cout << "Command : ";
+		std::cin >> cmd;
+		if (!is_valid_cmd(cmd))
+			std::cout << "wrong command!!";
+		else
+		{
+			if (cmd == "ADD")
+			{
+				handle_add(phonebook);
+			}
+			else if (cmd == "SEARCH") 
+			{
+				handle_search(phonebook);
+			}
+		}
+	}
 
 }
