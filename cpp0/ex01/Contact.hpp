eea27092048c7c4e2a05642464adc31befa79cba
Karam Koujan <kkoujan@student.1337.ma>
2025-07-02 05:02:28 +0100

    push

--- cpp0/ex01/Contact.hpp
+++ cpp0/ex01/Contact.hpp
@@ -2,14 +2,30 @@
 #define CONTACT_HPP
 #include <string>
 
+#include <string>
+
 class Contact
 {
-    public :
-        Contact();
-        std::string firstname;
-        std::string lastname;
-        std::string nickname;
-        std::string phone_number;
-        std::string darkest_secret;
+private:
+    std::string firstname;
+    std::string lastname;
+    std::string nickname;
+    std::string phone_number;
+    std::string darkest_secret;
+
+public:
+    Contact();
+    void setFirstName(const std::string& fname);
+    void setLastName(const std::string& lname);
+    void setNickname(const std::string& nname);
+    void setPhoneNumber(const std::string& phone);
+    void setDarkestSecret(const std::string& secret);
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getPhoneNumber() const;
+    std::string getDarkestSecret() const;
 };
+
+
 #endif
