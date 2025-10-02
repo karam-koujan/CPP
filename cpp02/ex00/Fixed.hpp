b050ceee76b7242021d83c4c3bf6b1e3f2883b3c
Karam Koujan <kkoujan@e3r2p16.1337.ma>
2025-10-02 12:15:58 +0100

    push origin

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 11:27:20 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/02 11:48:17 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/02 11:56:30 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class Fixed
         static const int _fraction_bits = 8;
     public :
         Fixed(): _integer(0) {};
-        Fixed(Fixed &other): _integer(0){};
+        Fixed(Fixed &other): _integer(other._integer){};
         Fixed &operator=(const Fixed &other);
         ~Fixed();
         int getRawBits (void) const;
