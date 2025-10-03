ba85e67bbf8925fe3b380d1bf5e4af78abde6db6
Karam Koujan <kkoujan@e1r7p7.1337.ma>
2025-10-03 11:34:26 +0100

    add float and number

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,17 +6,27 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 14:13:31 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/02 14:50:51 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/03 11:33:57 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
-
+#include <cmath>
 
 Fixed::Fixed():_integer(0)
 {
     std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int number)
+{
+    _integer <<= _fraction_bits;
+}
+
+Fixed::Fixed(const float number)
+{
+    _integer = roundf(number * (1 << _fraction_bits));
 }
 
 Fixed::~Fixed()
