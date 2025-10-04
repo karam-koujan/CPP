c3afd4eb2b445c35d73cd97917049a899d3ddbfe
Karam Koujan <kkoujan@e3r2p16.1337.ma>
2025-10-04 13:37:48 +0100

    define functions

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 14:13:31 by kkoujan           #+#    #+#             */
+/*   Updated: 2025/10/04 12:58:57 by kkoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed():_integer(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int number)
+{
+    _integer = number << _fraction_bits;
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float number)
+{
+    _integer = roundf(number * (1 << _fraction_bits));
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other): _integer(other._integer)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    if (this != &other)
+    {
+        std::cout << "Copy assignment operator called" << std::endl;
+        this->_integer = other.getRawBits();
+    }
+    return *this;    
+}
+
+int Fixed::getRawBits(void) const
+{
+    return _integer;
+}
+
+
+int Fixed::toInt(void) const
+{
+    return _integer >> _fraction_bits;
+}
+
+float Fixed::toFloat(void) const
+{
+    float result = (float)_integer / (1 << _fraction_bits);
+    return result;
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &obj)
+{
+    out << obj.toFloat();
+    return (out);
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 11:27:20 by kkoujan           #+#    #+#             */
+/*   Updated: 2025/10/04 13:20:20 by kkoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+class Fixed
+{
+    private :
+        int _integer;
+        static const int _fraction_bits = 8;
+    public :
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed(const int number);
+        Fixed(const float number);
+        Fixed &operator=(const Fixed &other);
+        ~Fixed();
+        bool operator<(const Fixed &other);
+        bool operator>(const Fixed &other);
+        bool operator>=(const Fixed &other);
+        bool operator<=(const Fixed &other);
+        bool operator==(const Fixed &other);
+        bool operator!=(const Fixed &other);
+        Fixed operator+(const Fixed &other);
+        Fixed operator-(const Fixed &other);
+        Fixed operator*(const Fixed &other);
+        Fixed operator/(const Fixed &other);
+        int getRawBits (void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt( void ) const;        
+};
+std::ostream &operator<<(std::ostream &out, Fixed const &obj);
+
+#endif
