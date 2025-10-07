d39cf27d33c5b8440f739b5792f3e17ad1170c14
Karam Koujan <kkoujan@e3r2p16.1337.ma>
2025-10-07 08:57:13 +0100

    remove comments

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 10:52:35 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/07 08:20:46 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/07 08:56:55 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 Point::Point(): x(0), y(0){};
 Point::Point(const float a, const float b): x(a), y(b){
-    // std::cout<< "copy constructor" << std::endl;
+    std::cout<< "copy constructor" << std::endl;
 };
 Point::Point(const Point &other): x(other.x), y(other.y){
-        // std::cout<< "default constructor" << std::endl;
+        std::cout<< "default constructor" << std::endl;
 };
 Point &Point::operator=(Point &other)
 {
@@ -26,7 +26,7 @@ Point &Point::operator=(Point &other)
 }
 Point::~Point()
 {
-    // std::cout<< "the obj is destroyed" << std::endl;
+    std::cout<< "the obj is destroyed" << std::endl;
 }
 float Point::getX() const
 {
