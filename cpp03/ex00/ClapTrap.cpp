ec3831a31dc4520b5dc9d31e9af94e2f1efb1677
Karam Koujan <kkoujan@e3r2p16.1337.ma>
2025-10-07 10:29:58 +0100

    untested first exercise

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 09:24:59 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/07 09:58:44 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/07 10:17:25 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,23 +46,33 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &other)
     return (*this);
 }
 
+ClapTrap::~ClapTrap()
+{
+    std::cout<< "destructors is called" << std::endl;
+}
+
 void ClapTrap::attack(const std::string& target)
 {
-    std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+    if (this->hitPoints > 0 && this->energyPoints > 0)
+    {
+        this->energyPoints--;        
+        std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+    }
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-        
+    this->hitPoints-= amount;
+    std::cout << "ClapTrap " << this->name << " took damage " << amount << ", causing " << this->attackDamage << " points of damage!" << std::endl;     
 }
 
-
-
-
-
-
-
-
-
-
-
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    
+    if (this->hitPoints > 0 && this->energyPoints > 0)
+    {
+        this->energyPoints--;
+        this->hitPoints+= amount;
+        std::cout << "ClapTrap " << this->name << " repairs itself and it gains " << amount << "of hits points, that costs " << std::endl;     
+    }
+}
