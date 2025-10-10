79eaceb51784431fc2935ace7c755ebeb5aa0561
Karam Koujan <kkoujan@e3r1p15.1337.ma>
2025-10-10 11:40:51 +0100

    push

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 09:54:56 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/10 11:13:49 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:34:38 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,18 @@ ScavTrap::ScavTrap()
     std::cout << "Default Constructor for ScavTrap" << std::endl;    
 }
 
+ScavTrap::~ScavTrap()
+{
+    std::cout << "Deconstructor for ScavTrap" << std::endl;      
+} 
+
 ScavTrap::ScavTrap(std::string n)
 {
     hitPoints = 100;
     energyPoints = 50;
     attackDamage = 20;
     name = n;
-    std::cout << "parametrized constructor is called" << std::endl;   
+    std::cout << "parametrized constructor is called of ScavTrap" << std::endl;   
 }
 
 ScavTrap::ScavTrap(const ScavTrap &other)
@@ -51,4 +56,9 @@ ScavTrap &ScavTrap::operator=(const ScavTrap &other)
         std::cout << "assignement operator of ScavTrap is called with name "<< this->name << std::endl;
     }
     return (*this);
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << "The ScavTrap is now in Gate keeper mode" << std::endl;
 }
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,19 +6,19 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 10:47:02 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/07 11:21:28 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:40:07 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 
 int main()
 {
-    // protect NULL
-    ClapTrap a = ClapTrap("first");
+    ScavTrap a = ScavTrap("first");
     a.attack("test");
     a.takeDamage(15);
     a.beRepaired(1);
     a.takeDamage(1);
+    a.guardGate();
 }
