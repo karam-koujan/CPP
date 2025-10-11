f38c6eeb9834fea1b8cd66654426ece735572021
Karam Koujan <kkoujan@e3r3p13.1337.ma>
2025-10-11 10:48:29 +0100

    add comments

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 10:47:02 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/10/11 10:14:10 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/10/11 10:47:51 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int main()
 {
+    /*
+    HitPoints : 100
+    EnergyPoints: 50
+    Attackdamage: 30
+    
+    */
     DiamondTrap a = DiamondTrap("first");
     a.attack("test");
     a.takeDamage(15);
