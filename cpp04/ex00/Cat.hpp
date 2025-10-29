f41b67392b4127f86bfc093dab406d5db895bd79
Karam Koujan <kkoujan@e3r2p13.1337.ma>
2025-10-29 14:35:58 +0100

    add cat

---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 14:34:41 by kkoujan           #+#    #+#             */
+/*   Updated: 2025/10/29 14:35:39 by kkoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_H
+#define CAT_H
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+    public :
+        Cat();
+        ~Cat();
+        Cat(const Animal &other);
+        Cat &operator=(const Animal &other);
+         
+};
+
+#endif
