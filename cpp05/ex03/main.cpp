91aa54becf900518b463f761e4e1575e6bf6e982
Karam Koujan <kkoujan@e1r9p1.1337.ma>
2025-12-16 18:28:33 +0100

    working on virtual destructor

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,24 +6,17 @@
 /*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 15:29:30 by kkoujan           #+#    #+#             */
-/*   Updated: 2025/12/16 16:37:38 by kkoujan          ###   ########.fr       */
+/*   Updated: 2025/12/16 18:24:36 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
-#include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 
 int main()
 {
-   try
-   {
-       Bureaucrat a = Bureaucrat("b1", 45);
-       RobotomyRequestForm f = RobotomyRequestForm("target");
-       a.signForm(f);
-       a.executeForm(f);
-   }
-   catch(const std::exception& e)
-   {
-        std::cerr << e.what() << std::endl;
-   }
+    Intern someRandomIntern;
+    AForm* rrf;
+    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+    delete rrf;
 }
