5bb3e9e5170920c6c9a59b9faa5e0df5c8bed833
karam-koujan <karamkaku2000@gmail.com>
2026-04-06 20:12:21 +0100

    add serialzier

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -6,7 +6,32 @@
 /*   By: kkoujan <kkoujan@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 20:01:40 by kkoujan           #+#    #+#             */
-/*   Updated: 2026/04/06 20:01:41 by kkoujan          ###   ########.fr       */
+/*   Updated: 2026/04/06 20:11:53 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Serializer.hpp"
+#include <iostream>
+
+Serializer::Serializer() {}
+
+Serializer::Serializer(const Serializer& other) {
+    *this = other; 
+}
+
+Serializer& Serializer::operator=(const Serializer& other) {
+    (void)other;
+    return *this;
+}
+
+Serializer::~Serializer() {}
+
+static uintptr_t Serializer::serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+static Data* Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data *>(raw);
+
+}
--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 19:48:34 by kkoujan           #+#    #+#             */
-/*   Updated: 2026/04/06 20:01:19 by kkoujan          ###   ########.fr       */
+/*   Updated: 2026/04/06 20:11:59 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ class Serializer
 {
     public :
 		static uintptr_t serialize(Data* ptr);
-		Data* deserialize(uintptr_t raw);
-		private :
+		static Data* deserialize(uintptr_t raw);
+	private :
         Serializer();
         Serializer(const Serializer &other);
         Serializer &operator=(const Serializer &other);
