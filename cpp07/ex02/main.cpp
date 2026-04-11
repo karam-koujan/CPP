74f447c0a7ff89c8dd703bd9fe46c7418a882273
karam-koujan <karamkaku2000@gmail.com>
2026-04-11 22:49:01 +0100

    finish ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkoujan <kkoujan@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 22:15:03 by kkoujan           #+#    #+#             */
-/*   Updated: 2026/04/11 22:33:33 by kkoujan          ###   ########.fr       */
+/*   Updated: 2026/04/11 22:47:44 by kkoujan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 int main()
 {
 	Array<int> *arr = new Array<int>(5);
+	Array<int> arr1 = *arr;
+	Array<int> arr2;
+	arr2 = *arr;
 	(*arr)[0] = 5;
 	std::cout << "size : " << arr->size() << std::endl;
 	try
 	{
-		std::cout << "element : " << (*arr)[0] << std::endl;
+		std::cout << "arr element : " << (*arr)[0] << std::endl;
+		std::cout << "arr1 element : " << arr1[0] << std::endl;
+		std::cout << "arr2 element : " << arr2[0] << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+
+	
 	delete arr;	
 }
