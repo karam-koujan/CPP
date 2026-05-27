91fc1f3dabe854fe55ba94407dc75968e3063d65
Karam Koujan <kkoujan@e3r1p3.1337.ma>
2026-05-27 06:42:36 +0100

    starting the execise

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkoujan <kkoujan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 06:32:12 by kkoujan           #+#    #+#             */
+/*   Updated: 2026/05/27 06:39:01 by kkoujan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <stack>
+
+class RPN
+{
+    private :
+        std::stack<int> s;
+    public :
+        RPN();
+        ~RPN();
+        RPN(const RPN &other);
+        RPN &operator=(const RPN &other);
+}
+
+#endif
